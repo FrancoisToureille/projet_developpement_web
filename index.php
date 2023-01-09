5764f67ae1a96db4059f0eed5dccf55a487e173b
François <92151472+FrancoisToureille@users.noreply.github.com>
2023-01-09 09:58:19 +0100

    premier squelette de l'index

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+function start_index() { 
+    ?>
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <title>Eclair d'Eugénie - Accueil</title> 
+            <!--<link rel="icon" type="image/x-icon" href="images/Bachelor.ico" sizes="96x96" /> -->
+            <!-- <script src="./javascript.js"></script> -->
+            <!-- <link rel="stylesheet" href="./css/styleAccueil.css"> -->
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+        </head>
+        <body>
+            <header>
+               
+            </header>
+            <div class="menu">
+                <div class="navBarr">
+                </div>
+            </div>
+
+            <div class="menuMobile">
+                <div class="navBarr">
+                </div>
+            </div>
+        
+            <footer>
+            
+            </footer>
+        </body>
+    </html>
+<?php } ?>
+
+<?php start_index();
+?>
