e2d6316ca622823abc026d787afc7dbb977642f6
François <92151472+FrancoisToureille@users.noreply.github.com>
2023-01-09 10:06:54 +0100

    ajout du squelette de detailRecette.php

---
+++ pages/detailRecette.php
@@ -0,0 +1,44 @@
+<?php 
+function start_detailRecette($recette) { 
+    ?>
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <title>Eclair d'Eugénie - Detail de la Recette</title> 
+            <!--<link rel="icon" type="image/x-icon" href="images/Bachelor.ico" sizes="96x96" /> -->
+            <!-- <script src="./javascript.js"></script> -->
+            <!-- <link rel="stylesheet" href="./css/styleAccueil.css"> -->
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+        </head>
+        <body>
+            <header>
+               
+            </header>
+            <div class="menu">
+                <div class="navBarr">
+                </div>
+                <div>
+                    <h1>Recette : <?php echo $recette; ?> </h1>
+                </div>
+            </div>
+
+            <div class="menuMobile">
+                <div class="navBarr">
+                </div>
+                <div>
+                    <h1>Recette : <?php echo $recette; ?> </h1>
+                </div>
+            </div>
+        
+            <footer>
+            
+            </footer>
+        </body>
+    </html>
+<?php } ?>
+
+<?php 
+$recette = "eclair d'Eugenie";
+start_detailRecette($recette);
+?>
