8a935277cb8a58334b0938804afc7d9da2b1747b
Francois <francois.toureille@etu.univ-amu.fr>
2023-01-09 10:30:22 +0100

    indentation corrigée

--- index.php
+++ index.php
@@ -2,34 +2,34 @@
 function start_index() { 
     ?>
 <!DOCTYPE html>
-    <html lang="fr">
-        <head>
-            <title>Eclair d'Eugénie - Accueil</title> 
+<html lang="fr">
+    <head>
+        <title>Eclair d'Eugénie - Accueil</title> 
             <!--<link rel="icon" type="image/x-icon" href="images/Bachelor.ico" sizes="96x96" /> -->
             <!-- <script src="./javascript.js"></script> -->
             <!-- <link rel="stylesheet" href="./css/styleAccueil.css"> -->
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-        </head>
-        <body>
-            <header>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    </head>
+    <body>
+        <header>
                
-            </header>
-            <div class="menu">
-                <div class="navBarr">
-                </div>
+        </header>
+        <div class="menu">
+            <div class="navBarr">
             </div>
+        </div>
 
-            <div class="menuMobile">
-                <div class="navBarr">
-                </div>
+        <div class="menuMobile">
+            <div class="navBarr">
             </div>
+        </div>
         
-            <footer>
+        <footer>
             
-            </footer>
-        </body>
-    </html>
+        </footer>
+    </body>
+</html>
 <?php } ?>
 
 <?php start_index();
