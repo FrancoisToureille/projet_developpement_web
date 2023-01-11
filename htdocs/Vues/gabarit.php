0daf9eb35d764ecc8d7748bddae207434c508b32
François <92151472+FrancoisToureille@users.noreply.github.com>
2023-01-11 14:15:41 +0100

    première version du projet en MVC

---
+++ htdocs/Vues/gabarit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Eclair d'Eugénie - Accueil</title> 
+            <!--<link rel="icon" type="image/x-icon" href="images/Bachelor.ico" sizes="96x96" /> -->
+            <!-- <script src="./javascript.js"></script> -->
+        <link rel="stylesheet" href="./style/styleIndex.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    </head>
+    <body>
+        <?php Vue::montrer('standard/entete'); ?>
+        <?php echo $A_vue['body'] ?>
+        <?php Vue::montrer('standard/pied'); ?>
+    </body>
+</html>
