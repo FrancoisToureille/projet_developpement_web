2fda2706932d017f3fb88a0935b85cf5854b092b
Veuveuno <98808815+alexisMariotti@users.noreply.github.com>
2023-01-13 10:10:21 +0100

    testing menuCategorie.php

--- htdocs/Modele/menuCategorie.php
+++ htdocs/Modele/menuCategorie.php
@@ -3,21 +3,39 @@
 class menuCategorie
 {
 
-    private $listeCategorie;
+    private $listeCategorie = array();
 
     public function __construct(){
-
+        $this->chargerCategorie();
     }
 
     private function chargerCategorie(){
         $connecteur = new ConnexionBDD(Constantes::nomUtilisateurBD(), Constantes::motDePAsseBD());
 
+        $requetteRecupererSuperCategorie = "SELECT idCategorie, nomCategorie FROM categorie WHERE idPere IS NULL";
 
+        $connecteur::donnerPdo()->prepare($requetteRecupererSuperCategorie);
+        $connecteur::donnerPdo()->execute();
+        $connecteur::donnerPdo()->setFetchMode(PDO::FETCH_OBJ);
 
+        $resultatSuperCategorie = $connecteur::donnerPdo()->fetchAll();
+
+        while ($superCategorie = $resultatSuperCategorie->fecth()){
+            $requetteRecupererSousCategorie = "SELECT idCategorie, nomCategorie FROM categorie WHERE idPere = $superCategorie->idCategorie";
+            $connecteur::donnerPdo()->prepare($requetteRecupererSousCategorie);
+            $connecteur::donnerPdo()->execute();
+            $listeSousCategorie = array();
+
+            while ($categorie = $connecteur::donnerPdo()->fetch()){
+                $listeSousCategorie[] = $categorie;
+            }
+
+            $this->listeCategorie[$superCategorie] = $listeSousCategorie;
+        }
     }
 
     public function donneListeCategorie(){
-
+        return $this->listeCategorie;
     }
 
 }
