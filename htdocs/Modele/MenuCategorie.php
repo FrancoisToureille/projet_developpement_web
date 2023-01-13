2a9b2dc192d27c6d8738af0cc0ee503227a8714d
Veuveuno <98808815+alexisMariotti@users.noreply.github.com>
2023-01-13 11:14:35 +0100

    replace refactor error in MenuCategorie.php sql queries

--- htdocs/Modele/MenuCategorie.php
+++ htdocs/Modele/MenuCategorie.php
@@ -12,7 +12,7 @@ class menuCategorie
     private function chargerCategorie(){
         $connexionBD = ConnexionBDD::getInstance();
 
-        $requetteRecupererSuperCategorie = "SELECT idCategorie, nomCategorie FROM menuCategorie WHERE idPere IS NULL";
+        $requetteRecupererSuperCategorie = "SELECT idCategorie, nomCategorie FROM categorie WHERE idPere IS NULL";
 
         $O_requette = $connexionBD->query($requetteRecupererSuperCategorie);
         $O_requette->setFetchMode(PDO::FETCH_OBJ);
@@ -20,7 +20,7 @@ class menuCategorie
         $resultatSuperCategorie = $O_requette->fetchAll();
 
         while ($superCategorie = $resultatSuperCategorie->fetch()){
-            $requetteRecupererSousCategorie = "SELECT idCategorie, nomCategorie FROM menuCategorie WHERE idPere = $superCategorie->idCategorie";
+            $requetteRecupererSousCategorie = "SELECT idCategorie, nomCategorie FROM categorie WHERE idPere = $superCategorie->idCategorie";
             $O_requetteSousCategorie = $connexionBD->query($requetteRecupererSousCategorie);
             $listeSousCategorie = array();
 
