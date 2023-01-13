dbce412b80638d4b4d4340ece544fbf5141c529f
Sloane <91048574+sloane26@users.noreply.github.com>
2023-01-13 09:33:57 +0100

    Changement variables en français

--- htdocs/Noyau/ConnexionBDD.php
+++ htdocs/Noyau/ConnexionBDD.php
@@ -4,18 +4,18 @@ class ConnexionBDD
     //Declaration of private variables
     private $O_pdo;
 
-    public function __construct($S_userName, $S_password)
+    public function __construct($S_nomUtilisateur, $S_motDePasse)
     {
         try {
             $S_dsn = 'mysql:S_host=mysql-eclair-d-eugenie.alwaysdata.net;S_dbName=eclair-d-eugenie_db';
-            $this->O_pdo = new PDO($S_dsn, $S_userName, $S_password); //Link with the database
+            $this->O_pdo = new PDO($S_dsn, $S_nomUtilisateur, $S_motDePasse); //Link with the database
         }
         catch (PDOException $e) {
             return $e->getMessage();
         }
     }
 
-    public static function getPdo() {
+    public static function donnerPdo() {
         return self::$O_pdo;
     }
 }
