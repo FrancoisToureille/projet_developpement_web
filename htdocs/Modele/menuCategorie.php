4dc2748da35255bce4aad5043ebc2fe92b5fa180
Veuveuno <98808815+alexisMariotti@users.noreply.github.com>
2023-01-13 09:23:01 +0100

    deleted ConnectionBDD.php

---
+++ htdocs/Modele/menuCategorie.php
@@ -0,0 +1,18 @@
+<?php
+
+class menuCategorie
+{
+
+    private $listeCategorie;
+
+    public function __construct(){
+
+    }
+
+    private
+
+    public function donneListeCategorie(){
+
+    }
+
+}
