96dbb5cefc0caa56537ce7f66e71891283798437
Sloane <91048574+sloane26@users.noreply.github.com>
2023-01-13 10:39:15 +0100

    Renomme statement en requete

--- htdocs/Modele/ConnexionUtilisateurBDD.php
+++ htdocs/Modele/ConnexionUtilisateurBDD.php
@@ -3,9 +3,9 @@ class ConnexionUtilisateurBDD {
     public function seConnecter($S_nomUtilisateur, $S_motDePasse) {
         $O_pdo = ConnexionBDD::getInstance();
         try {
-            $O_statement = $O_pdo->query("SELECT idUser FROM user WHERE name='" . $S_nomUtilisateur . "' AND password='" . $S_motDePasse ."'");
-            $O_statement->setFetchMode(PDO::FETCH_OBJ);
-            if ($O_statement->columnCount()) {
+            $O_requete = $O_pdo->query("SELECT idUser FROM user WHERE name='" . $S_nomUtilisateur . "' AND password='" . $S_motDePasse ."'");
+            $O_requete->setFetchMode(PDO::FETCH_OBJ);
+            if ($O_requete->columnCount()) {
                 return "Utilisateur connecté";
             }
             return "Nom d'utilisateur et/ou mot de passe incorrect(s)";
--- htdocs/Modele/InscriptionUtilisateurBDD.php
+++ htdocs/Modele/InscriptionUtilisateurBDD.php
@@ -4,16 +4,16 @@ class InscriptionUtilisateurBDD {
         $O_pdo = ConnexionBDD::getInstance();
         try {
             //On vérifie l'existance du nom dans la BDD
-            $O_statement = $O_pdo->query("SELECT idUser FROM user WHERE name='" . $S_nomUtilisateur . "'");
-            $O_statement->setFetchMode(PDO::FETCH_OBJ);
-            if ($O_statement->columnCount()) {
+            $O_requete = $O_pdo->query("SELECT idUser FROM user WHERE name='" . $S_nomUtilisateur . "'");
+            $O_requete->setFetchMode(PDO::FETCH_OBJ);
+            if ($O_requete->columnCount()) {
                 return "L'utilisateur existe déjà";
             }
             try {
                 //On vérifie l'existance de l'email
-                $O_statement = $O_pdo->query("SELECT idUser FROM user WHERE email='" . $S_email . "'");
-                $O_statement->setFetchMode(PDO::FETCH_OBJ);
-                if ($O_statement->columnCount()) {
+                $O_requete = $O_pdo->query("SELECT idUser FROM user WHERE email='" . $S_email . "'");
+                $O_requete->setFetchMode(PDO::FETCH_OBJ);
+                if ($O_requete->columnCount()) {
                     return "Email existe déjà";
                 }
                 //On vérifie la validité de l'email
@@ -21,8 +21,8 @@ class InscriptionUtilisateurBDD {
                 if (preg_match($S_email_exp, $S_email)) {
                     try {
                         //On crée un nouvel utilisateur dans la BDD
-                        $O_statement = $O_pdo->prepare("INSERT INTO user(nom,email,motDePasse) VALUES('" . $S_nomUtilisateur . "','" . $S_email . "','" . $S_motDePasse . "'");
-                        $O_statement->execute();
+                        $O_requete = $O_pdo->prepare("INSERT INTO user(nom,email,motDePasse) VALUES('" . $S_nomUtilisateur . "','" . $S_email . "','" . $S_motDePasse . "'");
+                        $O_requete->execute();
                         return;
                     }
                     catch (PDOException $e) {
