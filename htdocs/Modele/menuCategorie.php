eb32dfea75715dab822f7909b7ba3dec2a4f4a87
Clementine Henault <clementine.henault@etu.univ-amu.fr>
2023-01-13 10:48:29 +0100

    adaptation menuCategorie.php

--- htdocs/Modele/menuCategorie.php
+++ htdocs/Modele/menuCategorie.php
@@ -10,18 +10,18 @@ class menuCategorie
     }
 
     private function chargerCategorie(){
+        $connexionBD = ConnexionBDD::getInstance();
+
         $requetteRecupererSuperCategorie = "SELECT idCategorie, nomCategorie FROM categorie WHERE idPere IS NULL";
 
-        $O_requette = ConnexionBDD::donnerPdo()->prepare($requetteRecupererSuperCategorie);
-        $O_requette->execute();
+        $O_requette = $connexionBD->query($requetteRecupererSuperCategorie);
         $O_requette->setFetchMode(PDO::FETCH_OBJ);
 
         $resultatSuperCategorie = $O_requette->fetchAll();
 
         while ($superCategorie = $resultatSuperCategorie->fetch()){
             $requetteRecupererSousCategorie = "SELECT idCategorie, nomCategorie FROM categorie WHERE idPere = $superCategorie->idCategorie";
-            $O_requetteSousCategorie = ConnexionBDD::donnerPdo()->prepare($requetteRecupererSousCategorie);
-            $O_requetteSousCategorie->execute();
+            $O_requetteSousCategorie = $connexionBD->query($requetteRecupererSousCategorie);
             $listeSousCategorie = array();
 
             while ($categorie = $O_requetteSousCategorie->fetch()){
