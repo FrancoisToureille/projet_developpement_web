1ea60f7bc513d2beb69d891c1445f5e37505da34
Francois <francois.toureille@etu.univ-amu.fr>
2023-01-17 11:36:15 +0100

    requête permettant d'afficher toutes les recettes sans restrictions

--- htdocs/Controleurs/ControleurRecette.php
+++ htdocs/Controleurs/ControleurRecette.php
@@ -4,51 +4,40 @@ final class ControleurRecette
 {
     public function defautAction()
     {
-
-        $_O_recette =  new Recette("cake au citron", "melanger le tout"/*,array("farine","citron"),2*/);
+        $_O_recette =  new Recette("cake au citron", "melanger le tout","cat"/*,array("farine","citron"),2*/);
         Vue::montrer('recette/voir', array('recette' =>  $_O_recette->donneNomRecette()));
         Vue::montrer('recette/voir', array('recette' =>  $_O_recette->donneInstructions()));
-       
-        /*affichage des ingrédients d'une recette*/
-        /*$_I_nombre = $_O_recette->donneNombreIngredients();
-        foreach (range(0, $_I_nombre - 1) as $_I_indexIngredient) {
-            Vue::montrer('recette/voir', array('recette' =>  $_O_recette->donneIngredients($_I_indexIngredient)));
-
-        }
-        Vue::montrer('recette/voir', array('recette' =>  $_O_recette->donneDifficulte()));*/
-
     }
-
-
-    /*public function afficheIngredientAction()
-    {
-
-        $_O_recette =  new Recette("cake au citron", "melanger le tout",array("farine","citron"),2);
-       
-        affichage des ingrédients d'une recette
-        $_I_nombre = $_O_recette->donneNombreIngredients();
-        foreach (range(0, $_I_nombre - 1) as $_I_indexIngredient) {
-            Vue::montrer('recette/voir', array('recette' =>  $_O_recette->donneIngredients($_I_indexIngredient)));
-
-        }
-
-    }*/
 
     public function afficheRecettesAction()
     {
-        $_A_recettesBD = Recette::donneToutesLesRecettesBDD();
+        $_A_recettesBD = Recette::donneToutesRecettes();
         $_A_recettes = array();
         foreach (range(0, sizeof($_A_recettesBD) - 1) as $_I_indexRecette) {
-
-            $_A_recettes[$_I_indexRecette] = new Recette($_A_recettesBD[$_I_indexRecette]->nomRecette,$_A_recettesBD[$_I_indexRecette]->libelle);
+            $_A_recettes[$_I_indexRecette] = new Recette($_A_recettesBD[$_I_indexRecette]->nomRecette,
+            $_A_recettesBD[$_I_indexRecette]->libelle,
+            $_A_recettesBD[$_I_indexRecette]->categories,
+             $_A_recettesBD[$_I_indexRecette]->ingredients,
+              $_A_recettesBD[$_I_indexRecette]->quantites);
         }
        
         /*$_A_recettes = array(new Recette("cake au citron", "melanger le tout",array("farine","citron"),2),
         new Recette("cake aux olives", "melanger le tout",array("farine","olive"),2));*/
 
         foreach (range(0, sizeof($_A_recettes) - 1) as $_I_indexRecette) {
+            Vue::montrer('recette/voir', array('recette' => "recette:"));
             Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneNomRecette()));
-            Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneInstructions()));       
+            Vue::montrer('recette/voir', array('recette' => "ingredients:"));
+            Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneIngredients()));
+            Vue::montrer('recette/voir', array('recette' => "quantites:"));
+            Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneQuantites()));
+            Vue::montrer('recette/voir', array('recette' => "instructions:"));
+            Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneInstructions()));
+            Vue::montrer('recette/voir', array('recette' => "categories:"));
+            Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneNomCategories()));
+            Vue::montrer('recette/voir', array('recette' => "\n *******"));
+            
+
             /*$_I_nombre = $_A_recettes[$_I_indexRecette]->donneNombreIngredients();
             foreach (range(0, $_I_nombre - 1) as $_I_indexIngredient) {
                 Vue::montrer('recette/voir', array('recette' =>  $_A_recettes[$_I_indexRecette]->donneIngredients($_I_indexIngredient)));
