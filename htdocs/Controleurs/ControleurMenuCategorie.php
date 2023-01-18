cd78f795c7d641d8b7bf84df35a39cba6563b306
Veuveuno <98808815+alexisMariotti@users.noreply.github.com>
2023-01-18 12:09:14 +0100

    creation du controleur ControleurHome.php ainsi que de la vue home.php

--- htdocs/Controleurs/ControleurMenuCategorie.php
+++ htdocs/Controleurs/ControleurMenuCategorie.php
@@ -9,15 +9,7 @@ class ControleurMenuCategorie
         $O_MenuCategorie = new Categorie();
         $A_listeCategorie = $O_MenuCategorie->donneListeCategorie();
 
-        $S_affichageCategorie = "";
 
-        foreach ($A_listeCategorie as $superCategorie => $categorie){
-            $S_affichageCategorie .= "<li id='$superCategorie'> $superCategorie <ul>";
-            foreach ($categorie as $sousCategorie){
-                $S_affichageCategorie .= "<li id='$sousCategorie->idCategorie'> $sousCategorie->nomCategorie </li>";
-            }
-            $S_affichageCategorie .= "</ul> </li>";
-        }
 
         Vue::montrer('menuCategorie/menuCategorie', array('menuCategorie' => $S_affichageCategorie));
 
