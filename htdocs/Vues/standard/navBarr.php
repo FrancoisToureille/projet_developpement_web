e8337e554fbe6505f72048e2192f49e73c22fff7
Clementine Henault <clementine.henault@etu.univ-amu.fr>
2023-01-18 14:21:38 +0100

    nav bar recherche

--- htdocs/Vues/standard/navBarr.php
+++ htdocs/Vues/standard/navBarr.php
@@ -6,7 +6,9 @@
         <div id="accueil_txt"><button class="bouton lien" onclick="goToAccueil()"><span>Accueil</span></button></div>
     </div>
     <div class="search">
-        <input id="search_input" type="text" onclick="afficheRecherche()">
-        <img id="img_loupe" alt="img_loupe" src="/image/loupe.png">
+        <form action="/Recherche/rechercheRegex" method="POST">
+            <input id="search" type="text" name="search_input">
+            <input type="image" id="img_loupe" alt="img_loupe" src="./image/loupe.png">
+        </form>
     </div>
 </div>
