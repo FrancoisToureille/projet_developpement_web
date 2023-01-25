7b196755ac5b7799770310f8c0650c67aa2b3c5f
Clementine Henault <clementine.henault@etu.univ-amu.fr>
2023-01-25 22:10:06 +0100

    V0 vue et controleur pour l'insertion

--- htdocs/Controleurs/ControleurInsertion.php
+++ htdocs/Controleurs/ControleurInsertion.php
@@ -5,4 +5,24 @@ final class ControleurInsertion {
         $O_Categorie = new Categorie();
         Vue::montrer('insertion/insertion', array('listeSousCategorie' => $O_Categorie->donneListeSousCategorie(), 'listeIngredient' => Ingredient::donneTousLesIngredients()));
     }
+
+    public function enregistreRecetteAction() {
+        //Récupération des informations sur la recette
+        $S_nomRecette = $_POST['nom_recette'];
+        $S_libelle = $_POST['libelle'];
+        $S_lienImage = $_POST['lienImage'];
+        
+        $I_quantite = $_POST['quantite_ingredient']; //A changer pour mettre une quantite par ingredient
+
+        $A_ingredients = $_POST['ingredients'];
+        $A_categories = $_POST['categories'];
+
+        $A_idIngredients = array();
+
+        foreach ($A_ingredients as $key => $idIngredient) { //Construction du tableau pour la fonction d'insertion
+            $A_idIngredients[$idIngredient] = $I_quantite;
+        }
+
+        Recette::ajouterRecette($S_nomRecette, $S_libelle, $S_lienImage, $A_categories, $A_idIngredients);
+    }
 }
--- htdocs/Vues/insertion/insertion.php
+++ htdocs/Vues/insertion/insertion.php
@@ -3,29 +3,31 @@ $S_affichageCategories = "";
 $S_affichageIngredients = "";
 
 foreach ($A_vue['listeSousCategorie'] as $O_sousCategorie){
-    $S_affichageCategories .= "<option value=" . $O_sousCategorie->nomCategorie . ">";
+    $S_affichageCategories .= "<option name='categories[]' value=" . $O_sousCategorie->idCategorie . ">" . $O_sousCategorie->nomCategorie . "</option>";
 }
 
 foreach ($A_vue['listeIngredient'] as $O_ingredient) {
-    $S_affichageIngredients .= "<option value=" . $O_ingredient->libelle . ">";
+    $S_affichageIngredients .= "<option name='ingredients[]' value=" . $O_ingredient->idIngredient . ">" . $O_ingredient->libelle . "</option>";
 }
 
 echo "<div id='insertion'>
     <h1>Créer une nouvelle recette</h1>
-    <form id='form_insertion' action='/Insertion/defaut' method='post'>
+    <form id='form_insertion' action='/Insertion/enregistreRecette' method='post'>
         <label for='nom_recette'>Nom de la recette: </label>
         <input type='text' name='nom_recette'></br>
         <label for='choix_categorie'>Selectionnez une ou des catégories: </label>
-        <input list='liste_categories' id='choix_categorie' name=choix_categorie'>
-        <datalist id='liste_categories'>" . $S_affichageCategories .  
-        "</datalist></br>
-        <label for='choix_ingredient'>Selectionnez une ou des ingredients: </label>
-        <input list='liste_ingredients' id='choix_ingredient' name=choix_ingredient'>
-        <datalist id='liste_ingredients'>" . $S_affichageIngredients .  
+        <select multiple name='categories[]' id='liste_categories'>" . $S_affichageCategories .  
+        "</select></br>
+        <label for='choix_ingredient'>Selectionnez un ou des ingredients: </label>
+        <select name='ingredients[]' multiple id='liste_ingredients' style='width:6em'>" . $S_affichageIngredients .  
         // <!-- ingredients et ajouter si inexistant -->
-        "</datalist></br>
+        "</select>
+        <label for='quantite'>Quantite: </label>
+        <input type='text' name='quantite_ingredient'></br>
         <label for='libelle'>Instructions: </label>
         <input type='text' name='libelle'></br>
+        <label for='lienImage'>Lien de l'image de la recette (facultatif): </label>
+        <input type='text' name='lienImage'></br>
         <input type='submit' value='Enregistrer'>
     </form>
 </div>";
