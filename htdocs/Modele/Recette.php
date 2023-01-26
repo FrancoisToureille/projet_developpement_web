4026c4340f18d94bf3aecaa0d567d507a09fd3de
François <92151472+FrancoisToureille@users.noreply.github.com>
2023-01-26 17:10:04 +0100

    rajout d'une méthode supprimée par erreur

--- htdocs/Modele/Recette.php
+++ htdocs/Modele/Recette.php
@@ -88,6 +88,21 @@ final class Recette
             return $e->getMessage();
         }
     }
+    
+    /** méthode récupérant les noms et les id des recettes*/
+    public static function donneToutesLesRecettesNomId() {
+        $O_pdo = ConnexionBDD::getInstance()->getPdo();
+        try {
+            $O_statement = $O_pdo->query("SELECT nomRecette, idRecette FROM recette");
+            $O_statement->setFetchMode(PDO::FETCH_OBJ);
+            if ($O_statement->columnCount()) {
+                return $O_statement->fetchAll();
+            }
+        }
+        catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
 
     /**méthode recuperant les informations sur toutes les recettes existantes */
     public static function donneToutesRecettes() {
