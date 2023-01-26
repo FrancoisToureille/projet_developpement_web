a8420780ae009ea24fb6f3b45b1edc524565593f
NeekoInAbsentia <maxime.albertelli@etu.univ-amu.fr>
2023-01-26 18:43:05 +0100

    premiere tentative affichage css recette recherche

--- htdocs/Vues/recherche/afficheResult.php
+++ htdocs/Vues/recherche/afficheResult.php
@@ -1,7 +1,12 @@
 <?php
 
-
+$S_affichageResultat = "";
 foreach ($A_vue['listeRecetteRecherche'] as $O_recetteRecherche) {
-    echo "<a href='/Recette/afficheRecette/" . $O_recetteRecherche->idRecette . "'>" . $O_recetteRecherche->nomRecette . "</a>";
+    $S_affichageResultat .= "<a href='/Recette/afficheRecette/" . $O_recetteRecherche->idRecette . "'>" . $O_recetteRecherche->nomRecette . "</a>";
 }
 
+echo "
+<div id='afficheResult'>"
+    . $S_affichageCategorie .
+"</div>
+";
